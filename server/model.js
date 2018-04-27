56c00b676945b784006032535341511e6dae1f94
tidy.github.io <593243536@qq.com>
2018-04-27 18:01:32 +0800

    dsa

--- server/model.js
+++ server/model.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 // 链接mongo 并且使用imooc这个集合
-const DB_URL = 'mongodb://116.62.163.29:27017/imooc'
+const DB_URL = 'mongodb://localhost:27017/imooc'
 mongoose.connect(DB_URL)
 
 
